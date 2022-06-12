62a0cc1655accc87c2b65bfde487e4faf37aa9e8
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-06-12 22:10:15 +0500

    task controller get by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -23,7 +23,7 @@ export class TasksService {
     return task;
   }
 
-  getById(id: string): Task {
+  getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
   }
 }
