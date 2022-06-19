590555bc6fca6938e855f1f3046474d560890982
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-06-19 23:24:56 +0500

    signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth.credentials.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -9,4 +10,12 @@ export class AuthService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
+ 
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.usersRepository.create({ username, password });
+
+    await this.usersRepository.save(user);
+  }
 }
