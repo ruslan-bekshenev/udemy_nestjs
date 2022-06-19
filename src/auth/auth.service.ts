8c0653d5eee6448e092d4679103f81876e06c394
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-06-20 01:19:17 +0500

    sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -34,6 +35,17 @@ export class AuthService {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOneBy({ username });
+    const comparedPassword = await bcrypt.compare(password, user.password);
+    if (user && comparedPassword) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
     }
   }
 }
