ad12413ed174859ff004d3fbe1855cb0cddd38f8
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-06-20 00:11:16 +0500

    error handling username conficts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth.credentials.dto';
@@ -10,12 +14,19 @@ export class AuthService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
- 
+
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
     const user = this.usersRepository.create({ username, password });
-
-    await this.usersRepository.save(user);
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username is already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
