72d78c8a9bc2ab333cd0ac5388031830eae6d516
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-06-20 01:47:16 +0500

    add auth modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
