babbcf1f14398a836b4140a4e38aadc9ccfe724e
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-06-20 15:52:06 +0500

    remove test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,10 +18,4 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
-
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  test(@Req() req) {
-    console.log(req);
-  }
 }
